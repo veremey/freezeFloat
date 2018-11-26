35d2c1bc087aa908c48c7817b9bfbc4b9a03b460
Sergey Veremiychuk <veremey4uk@gmail.com>
2018-11-26 20:21:05 +0200

    for show

--- dev/temp/mocksData.js
+++ dev/temp/mocksData.js
@@ -87,12 +87,12 @@ _template: {
 	main: {
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float', name: 'Why Float'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services'},
+				{ link: '/freezeFloat/dev/float', name: 'Why Float'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us'}
+				{ link: '/freezeFloat/dev/about', name: 'about'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us'}
 			]},
 		]
 	},
@@ -100,12 +100,12 @@ _template: {
 		inner: 'header-inner',
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float', name: 'Why Float'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services'},
+				{ link: '/freezeFloat/dev/float', name: 'Why Float'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about', active: 'is-active'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us'}
+				{ link: '/freezeFloat/dev/about', name: 'about', active: 'is-active'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us'}
 			]},
 		]
 	},
@@ -113,12 +113,12 @@ _template: {
 		inner: 'header-inner',
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float', name: 'Why Float'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services', active: 'is-active'},
+				{ link: '/freezeFloat/dev/float', name: 'Why Float'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services', active: 'is-active'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us'}
+				{ link: '/freezeFloat/dev/about', name: 'about'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us'}
 			]},
 		]
 	},
@@ -126,12 +126,12 @@ _template: {
 		inner: 'header-inner',
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float', name: 'Why Float', active: 'is-active'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services'},
+				{ link: '/freezeFloat/dev/float', name: 'Why Float', active: 'is-active'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us'}
+				{ link: '/freezeFloat/dev/about', name: 'about'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us'}
 			]},
 		]
 	},
@@ -139,12 +139,12 @@ _template: {
 		inner: 'header-inner',
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/freeze', name: 'Why freeze', active: 'is-active'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services'},
+				{ link: '/freezeFloat/dev/freeze', name: 'Why freeze', active: 'is-active'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us'}
+				{ link: '/freezeFloat/dev/about', name: 'about'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us'}
 			]},
 		]
 	},
@@ -152,12 +152,12 @@ _template: {
 		inner: 'header-inner',
 		item: [
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/freeze', name: 'Why freeze'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/float-service', name: 'Services'},
+				{ link: '/freezeFloat/dev/freeze', name: 'Why freeze'},
+				{ link: '/freezeFloat/dev/float-service', name: 'Services'},
 			]},
 			{list: [
-				{ link: 'https://veremey.github.io/freezeFloat/dev/about', name: 'about'},
-				{ link: 'https://veremey.github.io/freezeFloat/dev/contact', name: 'contact us', active: 'is-active'}
+				{ link: '/freezeFloat/dev/about', name: 'about'},
+				{ link: '/freezeFloat/dev/contact', name: 'contact us', active: 'is-active'}
 			]},
 		]
 	}
